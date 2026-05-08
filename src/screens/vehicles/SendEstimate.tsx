bea654f75d353d7ef1f4d82bde92b3525e3afc57
fullstackdev1008 <fullstackdev1008@gmail.com>
2026-05-08 11:36:29 +0530

    Update SendEstimate.tsx

--- src/screens/vehicles/SendEstimate.tsx
+++ src/screens/vehicles/SendEstimate.tsx
@@ -104,10 +104,6 @@ export const SendEstimate = () => {
         if (d.emailSent) {
           toast.success("Estimate sent to customer via email");
         } 
-        // else {
-        //   const reason = d.emailFailReason || "check SMTP settings";
-        //   toast.error(`Email not sent: ${reason}`);
-        // }
 
         if (d.whatsappSent) {
           toast.success("Estimate sent to customer via WhatsApp");
